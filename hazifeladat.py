f39e6cd78d5991cf845aa7f50929c92fdc11d40e
vandussuto <36521934+vandussuto@users.noreply.github.com>
2018-02-26 00:10:39 +0100

    Add files via upload

--- hazifeladat.py
+++ hazifeladat.py
@@ -7,6 +7,8 @@ def feladat_7(h,sz,d):
     elif maradek<0:
         print (maradek*-1,"m drótra van még szükség.")
 
+
+
 def feladat_11(ev):
     if ev//4==0:
         szokoev=ev/4
@@ -18,4 +20,31 @@ def feladat_11(ev):
         szokoev=y/4
         nap=int(szokoev*366+(ev-szokoev+x)*365)
         print(nap)
-        
+
+
+
+def feladat_14(h):
+    if h==1:
+        print("Január")
+    elif h==2:
+        print("Február")
+    elif h==3:
+        print("Március")
+    elif h==4:
+        print("Április")
+    elif h==5:
+        print("Május")
+    elif h==6:
+        print("Június")
+    elif h==7:
+        print("Július")
+    elif h==8:
+        print("Augusztus")
+    elif h==9:
+        print("Szeptember")
+    elif h==10:
+        print("Október")
+    elif h==11:
+        print("November")
+    elif h==12:
+        print("December")
